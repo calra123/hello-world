cf41db3e83f77830bc51c14dee9b70cc70a6125f
calra123 <32410407+calra123@users.noreply.github.com>
2018-09-03 17:12:57 +0530

    template function in c++

---
+++ template_func.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+template <typename t>
+t square(t x)
+{
+	return x*x;
+}
+template <typename t>
+class Bovector{
+	t arr[1000];
+	int size;
+	public:
+	
+}
+
+int main() {
+	cout<<square('1');
+	// your code goes here
+	return 0;
+}
